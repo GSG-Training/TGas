f33e2aa26cd1be274196e8c4a2fab805a8cbf9c9
nour terry <nourhani1561999@gmail.com>
2020-11-30 21:47:54 +0200

    add drawer layout in home screen

--- app/src/main/java/com/example/project_test/HomeActivity.java
+++ app/src/main/java/com/example/project_test/HomeActivity.java
@@ -1,14 +1,23 @@
 package com.example.project_test;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
 
 public class HomeActivity extends AppCompatActivity {
-
+   DrawerLayout drawerLayout;
+   Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        drawerLayout=findViewById(R.id.drawer_home);
+        toolbar=findViewById(R.id.toolbar_home);
+        setSupportActionBar(toolbar);
+        ActionBarDrawerToggle actionBarDrawerToggle=new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.open,R.string.close);
+        actionBarDrawerToggle.syncState();
     }
 }
